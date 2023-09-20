49282369e21b11499ea91c49f4140ce924637546
ArianMathai <123459777+ArianMathai@users.noreply.github.com>
2023-09-20 15:32:36 +0200

    Initial commit of heroku project

---
+++ client/index.jsx
@@ -0,0 +1,5 @@
+import react from "react";
+import ReactDOM from "react-dom/client"
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<h1>Hey</h1>)
---
+++ server/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+app.use(express.static("../client/dist"));
+
+app.get("/api/tasks", (req, res) => {
+    res.json([
+        {
+           id: 1,
+           title: "get server working",
+           status: "todo"
+        },
+        {
+            id: 2,
+            title: "get heroku working",
+            status: "todo"
+        },
+        {
+            id: 3,
+            title: "ball out of control",
+            status: "todo"
+        }
+    ])
+})
+
+
+app.listen(process.env.PORT || 3000);
